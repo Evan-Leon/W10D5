81443e9cfcbd86fafa23cc3e5612c8641e7faafb
Evan-Leon <evan.leon.j@gmail.com>
2021-05-02 19:54:32 -0400

    Added components folder with app and root files

---
+++ todos_2/frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => (
+
+    <div>
+        <h1>The Todo App</h1>
+        
+    </div>
+    
+)
+
+export default App;
--- todos_2/frontend/todo_redux.jsx
+++ todos_2/frontend/todo_redux.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store'
 import {receiveTodo, receiveTodos, removeTodo} from "./actions/todo_actions";
 import {receiveStep, receiveSteps, removeStep} from "./actions/step_actions";
+import App from '../frontend/components/app.jsx';
+import Root from '../frontend/components/root.jsx';
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -19,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.receiveSteps = receiveSteps;
     window.removeStep = removeStep;
 
-    ReactDOM.render(<h1>Todos App</h1>, root);
+    ReactDOM.render(<Root />, root);
 
 
 });
